a90d875fbc820475a01084c10b555b7527ce62f4
Luca-Orabona <lucaorabona94@gmail.com>
2025-10-27 10:38:24 +0100

    Setup Context API e Fetch Iniziale

---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const GlobalContext = createContext();
+
+export const GlobalProvider = ({children}) => {
+    const urlAPI = import.meta.env.VITE_URL_API
+
+    const [task, setTask] = useState();
+
+    const getTask = async () => {
+        try {
+            const resp = await fetch(`${urlAPI}/tasks`);
+            const data = await resp.json();
+            setTask(data)
+            console.log(data);
+            
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getTask()
+    }, [])
+
+
+    const value= {
+        task
+    }
+
+    return (
+        <GlobalContext.Provider value={value}>
+            {children}
+        </GlobalContext.Provider>
+    )
+};
+
+export const useGlobalContext = () => useContext(GlobalContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GlobalProvider } from './context/GlobalContext.jsx';
 import "bootstrap/dist/css/bootstrap.css";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  //<StrictMode>
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
+  //</StrictMode>,
 )

705d87b40c4b5d7753f96e2b2d3043c8890d1ad3
Luca-Orabona <lucaorabona94@gmail.com>
2025-10-28 11:18:10 +0100

    Aggiunto modale per conferma eliminazione task

---
+++ src/components/ModalDelete.jsx
@@ -0,0 +1,35 @@
+import { forwardRef } from "react";
+import ReactDOM from "react-dom";
+
+const ModalDelete = forwardRef(function Modal(
+  { title, show, onConfirm = () => { }, onClose = () => { } },
+  ref
+) {
+  if (!show) return null;
+
+  return ReactDOM.createPortal(
+    <div className="position-fixed top-0 start-0 w-100 h-100 bg-dark bg-opacity-75 d-flex justify-content-center align-items-center z-3">
+      <div className="bg-white rounded-4 shadow-lg p-4 text-center" style={{ maxWidth: "450px", width: "90%" }} ref={ref}>
+        {/* Titolo */}
+        <h5 className="fw-bold mb-3">{title}</h5>
+
+        {/* Azioni */}
+        <div className="d-flex justify-content-center gap-4 mt-3">
+
+          <button type="button" className="btn btn-secondary px-4" onClick={onClose}>
+            Annulla
+          </button>
+
+          <button type="button" className="btn btn-danger px-4" onClick={onConfirm}>
+            Elimina
+          </button>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+});
+
+export default ModalDelete;
+
+
--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,10 +1,17 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../context/GlobalContext";
+import ModalDelete from "../components/ModalDelete"
+import { useRef, useState } from "react";
 
 const TaskDetail = () => {
-  const { tasks, removeTask} = useGlobalContext();
+  const { tasks, removeTask } = useGlobalContext();
   const { id } = useParams();
   const navigate = useNavigate();
+
+
+  //modale elimina task
+  const [showModal, setShowModal] = useState(false);
+  const modalRef = useRef();
 
   const task = tasks.find((t) => t.id === Number(id));
 
@@ -30,45 +37,57 @@ const TaskDetail = () => {
   }
 
   return (
-    <section className="d-flex justify-content-center align-items-start min-vh-100 bg-light pt-5">
-      <div className="card shadow-lg border-0 rounded-4 w-100" style={{ maxWidth: "600px" }}>
-        <div className="card-body p-4">
-          <h1 className="card-title text-center mb-4 fw-bold text-dark">
-            {task.title}
-          </h1>
+    <>
+      <section className="d-flex justify-content-center align-items-start min-vh-100 bg-light pt-5">
+        <div className="card shadow-lg border-0 rounded-4 w-100" style={{ maxWidth: "600px" }}>
+          <div className="card-body p-4">
+            <h1 className="card-title text-center mb-4 fw-bold text-dark">
+              {task.title}
+            </h1>
 
-          <div className="mb-3">
-            <p className="fw-semibold mb-1">Descrizione:</p>
-            <p className="p-3 bg-body-secondary rounded-3 text-secondary small mb-0">
-              {task.description}
-            </p>
+            <div className="mb-3">
+              <p className="fw-semibold mb-1">Descrizione:</p>
+              <p className="p-3 bg-body-secondary rounded-3 text-secondary small mb-0">
+                {task.description}
+              </p>
+            </div>
+
+            <div className="mb-3">
+              <p className="fw-semibold mb-1">Stato:</p>
+              <span
+                className={`badge ${getStatusClass(task.status)} px-3 py-2 rounded-pill text-uppercase fw-semibold`}
+              >
+                {task.status}
+              </span>
+            </div>
+
+            <div className="mb-3">
+              <p className="fw-semibold mb-1">Creata il:</p>
+              <p className="text-secondary mb-0 small">
+                {new Date(task.createdAt).toLocaleString()}
+              </p>
+            </div>
+
+            <button onClick={() => setShowModal(true)}  className="btn btn-danger w-100 mt-3 fw-semibold">
+              Elimina Task
+            </button>
           </div>
-
-          <div className="mb-3">
-            <p className="fw-semibold mb-1">Stato:</p>
-            <span
-              className={`badge ${getStatusClass(task.status)} px-3 py-2 rounded-pill text-uppercase fw-semibold`}
-            >
-              {task.status}
-            </span>
-          </div>
-
-          <div className="mb-3">
-            <p className="fw-semibold mb-1">Creata il:</p>
-            <p className="text-secondary mb-0 small">
-              {new Date(task.createdAt).toLocaleString()}
-            </p>
-          </div>
-
-          <button onClick={() => {
-            removeTask(id);
-            navigate(`/`);
-          }} className="btn btn-danger w-100 mt-3 fw-semibold">
-            Elimina Task
-          </button>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <ModalDelete
+        ref={modalRef}
+        show={showModal}
+        title="Sei sicuro di voler eliminare la task?"
+        onClose={() => setShowModal(false)}
+        onConfirm={() => {
+          removeTask(id);
+          setShowModal(false);
+          navigate("/");
+        }}
+      />
+
+    </>
   );
 };
 
